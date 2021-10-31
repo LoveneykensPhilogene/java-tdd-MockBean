efe1f53c6fc428c6c2413c471c43d7af9908833d
Loveneykens Philogene <loveneykens@gmail.com>
2021-10-31 18:29:00 -0300

    atualizado!

--- src/test/java/br/testeunitario/BookingServiceTest.java
+++ src/test/java/br/testeunitario/BookingServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
@@ -19,43 +18,41 @@ import br.testeunitario.repository.BookingRepository;
 import br.testeunitario.service.BookingService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class BookingServiceTest {
-	
+
 	@TestConfiguration
-	static class BookingServiceTestConfiguration{
-		
+	static class BookingServiceTestConfiguration {
+
 		@Bean
 		public BookingService bookingService() {
-		
-		return new BookingService();
+
+			return new BookingService();
 		}
 	}
-	
-	
+
 	@Autowired
 	BookingService bookingService;
-	
+
 	@MockBean
 	BookingRepository bookingRepository;
-	
+
 	@Test
 	public void bookingTestServiceDaysCalculator() {
-		
-		String name="Love";
-		int days=bookingService.bookingDaysCalculatorWithDatabase(name);
-		Assertions.assertEquals(days,10);
+
+		String name = "Love";
+		int days = bookingService.bookingDaysCalculatorWithDatabase(name);
+		Assertions.assertEquals(days, 10);
 	}
 
 	@Before
 	public void setup() {
-		LocalDate checkIn=LocalDate.parse("2020-11-10");
-		LocalDate checkOut=LocalDate.parse("2020-11-20");
-		BookingModel bookingModel=new BookingModel("1","Love",checkIn,checkOut,2);
-				
+		LocalDate checkIn = LocalDate.parse("2020-11-10");
+		LocalDate checkOut = LocalDate.parse("2020-11-20");
+		BookingModel bookingModel = new BookingModel("1", "Love", checkIn, checkOut, 2);
+
 		Mockito.when(bookingRepository.findByReserveName(bookingModel.getReserveName()))
-		.thenReturn(java.util.Optional.of(bookingModel));
-		
+				.thenReturn(java.util.Optional.of(bookingModel));
+
 	}
 
 }
